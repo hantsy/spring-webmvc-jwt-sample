d78aaafe51e4f5cffb5a797b1c9387e7db17fda9
hantsy <hantsy@gmail.com>
2019-03-16 10:18:43 +0800

    Upgraded to Spring Boot 2.1.3.RELEASE, and fixes #1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,15 +35,11 @@ class DataJpaConfig {
 
     @Bean
     public AuditorAware<User> auditor() {
-        return () -> {
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-            if (authentication == null || !authentication.isAuthenticated()) {
-                return Optional.empty();
-            }
-
-            return Optional.of((User) authentication.getPrincipal());
-        };
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+            .map(SecurityContext::getAuthentication)
+            .filter(Authentication::isAuthenticated)
+            .map(Authentication::getPrincipal)
+            .map(User.class::cast);
     }
 }
 
